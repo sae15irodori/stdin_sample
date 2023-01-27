d09308cca688da7239d341095db3f834350c3bd4
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-01-27 02:05:49 +0000

    二次元配列要素の取り出し方

--- print_2d_array.rb
+++ print_2d_array.rb
@@ -1,11 +1,14 @@
+#２次元配列 配列の中に配列がある
 arr = [
-  ['A', 'B', 'C'],
-  ['D', 'E', 'F'],
-  ['G', 'H', 'I']
+  ['A', 'B', 'C'],#１
+  ['D', 'E', 'F'],#２
+  ['G', 'H', 'I'] #３
 ]
 
-arr.each do |inner_arr|
-  inner_arr.each do |word|
-    p word
+arr.each do |inner_arr|#親配列の中身を順に取り出して格納１，２，３
+  inner_arr.each do |word|#１配列の中身を順に取り出して格納A,B,C
+    p word#A B Cを出力。１つ上の行に戻り２配列の中身を取り出し...となる。
   end
 end
+
+#eachの中でeachすることで、二次元配列に対応できる
