6bba66e98969a28be94e6bb2b38b347ddee5f60a
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-01-27 02:19:58 +0000

    複数行を配列として出力する

--- readlines_practice.rb
+++ readlines_practice.rb
@@ -1,5 +1,7 @@
-lines = readlines
-lines.each do |words|
-  p words.chomp.split(' ')
+lines = readlines#複数行の標準入力させて、linesに配列として格納[A a,B b,C c]行ごとに,区切り
+lines.each do |words|#入力されたものを順に取り出しwordsに格納。1つ目A a ２つ目B b ３つ目C c
+  p words.chomp.split(' ')#半角ｽﾍﾟｰｽを区切りとして配列にして出力。１つ目[A,a] ２つ目[B,b] ３つ目[C,c]
 end
 
+#chompメソッドは、標準入力時の末尾の改行を取り払うメソッド
+#必ず必要なわけじゃない。状況に応じて使う
